0307a714ed2b1c3c04a7957857c92ec65698448c
albashaalbasha4454-sudo <albasha.albasha4454@gmail.com>
2026-05-02 21:48:05 +0300

    Seed required expenses before app starts

--- index.tsx
+++ index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css';
+import { installExpenseTransactionSync, seedRequiredExpenses } from './expenseSeeds';
+
+installExpenseTransactionSync();
+seedRequiredExpenses();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
