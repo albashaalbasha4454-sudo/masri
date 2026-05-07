a6f353a824090c704fd1e4be93a006261f76d0a7
albashaalbasha4454-sudo <albasha.albasha4454@gmail.com>
2026-05-07 03:02:30 +0300

    Launch unified rebuilt app

--- index.tsx
+++ index.tsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import UnifiedApp from './UnifiedApp';
 import './style.css';
-import { installExpenseTransactionSync, seedRequiredExpenses } from './expenseSeeds';
-import { seedRequiredProducts } from './productSeeds';
-import { seedDrawerAccountAndCashMovements } from './cashMovementSeeds';
-import { applyUniqueProductImages } from './productImagePatcher';
 import { installUiTextPatcher } from './uiTextPatcher';
-import { installDataBridge, syncAllBusinessData } from './dataBridge';
 import { installProfessionalReportExportPatcher } from './reportExportPatcher';
 
 installUiTextPatcher();
-installExpenseTransactionSync();
-installDataBridge();
 installProfessionalReportExportPatcher();
-seedRequiredExpenses();
-seedRequiredProducts();
-seedDrawerAccountAndCashMovements();
-syncAllBusinessData();
-applyUniqueProductImages();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <UnifiedApp />
   </React.StrictMode>
 );
